1a773ca0779a759bd7511d20159481419cbf68db
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-06-21 01:36:14 +0300

    react

---
+++ reactjs/src/Hedaer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Hedaer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Horns Gallery</h1>
+            </div>
+        )
+    }
+}
+
+export default Hedaer
---
+++ reactjs/src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Beast1 from './Beast1'
+import Beast2 from './Beast2'
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+               <Beast1/>
+               <Beast2/> 
+            </div>
+        )
+    }
+}
+
+export default Main
